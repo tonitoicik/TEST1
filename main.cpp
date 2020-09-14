74a83afa4e40b690f7e123f28c4e1bc117b54563
tonitoicik <54882998+tonitoicik@users.noreply.github.com>
2020-09-14 10:44:57 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a,b,c = 0;
+  int arr[2] = {0, 0};
+  cout <<" Милорд, введите первое, а затем второе число через пробел"<<endl;
+  cin >> a >> b; 
+  c=a;
+  a=b;
+  b=c;
+  cout<< "первое число теперь = "<< a <<endl<< "второе число теперь = " << b<<endl<< "А теперь по другому"<<endl;
+  
+ 
+
+}
